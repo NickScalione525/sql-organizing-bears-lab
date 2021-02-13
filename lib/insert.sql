86cbd7b1307d4c3736901b2595c1a9d1707b8cc8
nick <nicholas_scalione@yahoo.com>
2021-02-12 22:09:45 -0500

    commit tree 853b5c6cccef2f05a5eb830337fc4232fc55751d

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 34, "M", "Brown", "Promiscuous", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 8, "M", "Green", "Wild", 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 2, "F", "Brown", "Happy", 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergant Brown", 25, "M", "Light Brown", "Mean", 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 10, "F", "Black", "Adventerous", 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 200, "M", "Blue", "Sad", 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 34, "F", "Brown", "evil", 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( NULL, 34, "M", "Brown", "Promiscuous", 1 );
